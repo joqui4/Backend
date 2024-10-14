e33e2ad32b3c68784ec194d4e94ba393738c36c3
CodAres <u202122633@upc.edu.pe>
2024-10-14 17:42:06 -0500

    feat: add discovery-server microservice

---
+++ services/discovery-server/src/test/java/com/codares/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codares.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

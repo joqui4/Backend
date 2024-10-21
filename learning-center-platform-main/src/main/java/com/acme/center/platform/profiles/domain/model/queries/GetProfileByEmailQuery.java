55fa62d4435d5a543dfbf0293c8d6a0ec9521030
SebastiaNCastro22 <u20181c241@upc.edu.pe>
2024-10-20 20:53:23 -0500

    feet: BC-Learnning

---
+++ learning-center-platform-main/src/main/java/com/acme/center/platform/profiles/domain/model/queries/GetProfileByEmailQuery.java
@@ -0,0 +1,5 @@
+package com.acme.center.platform.profiles.domain.model.queries;
+
+import com.acme.center.platform.profiles.domain.model.valueobjects.EmailAddress;
+
+public record GetProfileByEmailQuery(EmailAddress email) { }
